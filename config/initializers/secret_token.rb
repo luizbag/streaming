e5474b7c83e2647ea20ef6bb518512db858d00c0
Luiz Gavioli <luizbag@gmail.com>
2014-09-01 16:29:15 -0300

    Starting streaming project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Streaming::Application.config.secret_key_base = 'c1f13abc4bda14b6fc82f66ddc7057893d96c368f0b77bdf483e72f5cf975a3860c5e9d2a063e8794109d8bb9a70b286ba5c2ac397568aa5b39c1a4506ade8ef'
